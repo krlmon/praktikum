37595b801eaf1b9b8acb23de21cfa93e4d033ae2
krlmon <92529010+krlmon@users.noreply.github.com>
2021-11-12 10:13:53 +0700

    Add files via upload

---
+++ MODUL III/PendaftaranLomba/src/pendaftaranlomba/LombaEntity_90476.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pendaftaranlomba;
+
+/**
+ *
+ * @author Administrator
+ */
+public class LombaEntity_90476 {
+    public static String[] Lomba = {"makan Kerupuk", "Panjat Pinang", 
+        "Lari Marathon"};
+}
